795828aed79388560f13d0bcd510d5b899b0719c
kow <kaionesyan@gmail.com>
2022-04-06 20:36:00 -0300

    update payments mongo connection

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -41,7 +41,11 @@ const start = async () => {
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'auth',
+      retryWrites: true,
+      w: 'majority',
+    });
 
     console.log('App connected to the database');
   } catch (error) {
